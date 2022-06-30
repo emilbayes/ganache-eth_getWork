078a155be0fbd8729c50d6b47e54545085a375f3
MicaiahReid <micaiahreid@gmail.com>
2022-06-30 16:40:41 -0400

    set caller address for state trie context

--- src/chains/ethereum/ethereum/src/helpers/simulation-handler.ts
+++ src/chains/ethereum/ethereum/src/helpers/simulation-handler.ts
@@ -122,10 +122,12 @@ export default class SimulationHandler {
     const blockchain = this.#blockchain;
     const common = this.#common;
 
+    const caller = this.toLightEJSAddress(transaction.from);
+
     const stateTrie = blockchain.trie.copy(false);
     stateTrie.setContext(
       simulationBlock.header.stateRoot.toBuffer(),
-      null,
+      caller.buf,
       simulationBlock.header.number
     );
     this.#stateTrie = stateTrie;
@@ -157,8 +159,6 @@ export default class SimulationHandler {
 
     if (gasLeft >= 0n) {
       this.#setupStepEventEmits();
-
-      const caller = this.toLightEJSAddress(transaction.from);
 
       if (common.isActivatedEIP(2929)) {
         const precompiles = this.#warmDefaults(caller, to);
